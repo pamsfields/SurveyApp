19424def256f9a0f231eaf3e8b22bd7854fac8bc
pamsfields <jh5811zj@go.minneapolis.edu>
2017-09-16 17:10:40 -0500

    Updated values, but still not able to work. Project crashes on attempt to update counts and display information.

--- app/src/main/java/com/pam/surveyapp/SurveyApp.java
+++ app/src/main/java/com/pam/surveyapp/SurveyApp.java
@@ -13,26 +13,37 @@ public class SurveyApp extends AppCompatActivity {
     private Button mYesButton;
     private Button mNoButton;
     private TextView mQuestionTextView;
-    //private Int yesCount;
-    //private Int noCount;
+    private int yesCount=0;
+    private int noCount=0;
+    private TextView no_Counter;
+    private TextView yes_Counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_app);
 
+        mQuestionTextView= (TextView) findViewById(R.id.question_text_view);
+        yes_Counter= (TextView) findViewById(R.id.yes_Counter);
+        no_Counter= (TextView) findViewById(R.id.no_Counter);
+
         mYesButton = (Button) findViewById(R.id.yes_button);
         mYesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Need to add a counter to this portion, something like yesCount++;
+                yesCount++;
+                Toast.makeText(SurveyApp.this, "Thank You!", Toast.LENGTH_SHORT).show();
+                yes_Counter.setText(+Integer.toString(yesCount));
             }
         });
         mNoButton = (Button) findViewById(R.id.no_button);
         mNoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Need to add a counter to this portion, something like noCount++;
+                noCount++;
+                Toast.makeText(SurveyApp.this, "Thank You!", Toast.LENGTH_SHORT).show();
+
+                no_Counter.setText(++Integer.toString(noCount));
             }
         });
     }
