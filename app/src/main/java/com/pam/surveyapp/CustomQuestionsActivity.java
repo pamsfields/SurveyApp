4ccead142410fdf61fcd2813796327f50c9aeabe
pamsfields <jh5811zj@go.minneapolis.edu>
2017-09-24 18:42:34 -0500

    Project pulls values but shows code response to counts unable to pull up results window Added customization question option

---
+++ app/src/main/java/com/pam/surveyapp/CustomQuestionsActivity.java
@@ -0,0 +1,15 @@
+package com.pam.surveyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_questions);
+
+
+    }
+}
